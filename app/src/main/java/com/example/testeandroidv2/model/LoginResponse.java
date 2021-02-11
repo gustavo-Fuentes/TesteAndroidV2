4d98d1dfb09c29d768568ea71055fd5233e3c645
gustavo-Fuentes <fuentesfei99@gmail.com>
2021-02-10 21:38:22 -0300

    classe resposta da chama do UserService

--- app/src/main/java/com/example/testeandroidv2/model/LoginResponse.java
+++ app/src/main/java/com/example/testeandroidv2/model/LoginResponse.java
@@ -1,23 +1,50 @@
 package com.example.testeandroidv2.model;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 public class LoginResponse {
 
-    @SerializedName("user")
-    @Expose
-    private String user;
+    private String userId;
+    private String name;
+    private String bankAccount;
+    private String agency;
+    private String balance;
 
-    @SerializedName("password")
-    @Expose
-    private String password;
-
-    public String getUser() {
-        return user;
+    public String getUserId() {
+        return userId;
     }
 
-    public String getPassword() {
-        return password;
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
     }
 }
